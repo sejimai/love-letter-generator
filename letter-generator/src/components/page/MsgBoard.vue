78156d75b7ca44d591725b0a58c33fe79365a325
sejimai <3211943963@qq.com>
2021-12-11 23:48:50 +0800

    [BP] complete letter generator page
    I complete the letter generator page, and try to change the style. But
    it doesn't work.

---
+++ letter-generator/src/components/page/MsgBoard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <i class="el-icon-lx-cascades"></i> 留言板
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="common-container">
+      <div class="container">
+        <div class="msg-module">
+          <a>内容一</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fetchData } from "../../api/index";
+import Axios from "axios";
+import Vue from "vue";
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this.getPage();
+  },
+  methods: {
+    getPage() {},
+  },
+};
+</script>
+
+<style>
+</style>
